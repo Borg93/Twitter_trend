d62bf150e98fceb0852abe67f406252be71117d7
Borg93 <48671678+Borg93@users.noreply.github.com>
2020-08-09 20:00:06 +0200

    Add files via upload

---
+++ Twitter_trend-master/twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "11159820778612908033-Gwz49gLeq7B4MZWVC0SWTjV1UeGiBu"
+ACCESS_TOKEN_SECRET = "5y8mbcCFJNR4dnHhoHkeiqqaxoEWN6P6q6eLXBefcnXpc"
+CONSUMER_KEY = "RjKZq1rwPhJhblGOb5iVxC35E"
+CONSUMER_SECRET = "ISWWxs79g12mKljZ9tKv0bfKCxhcQnzVnGdEzvv1wVdjzxm1GR"
